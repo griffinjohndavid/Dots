7d323e69eaee906a70e7f31b03c79ad8935956ba
John Griffin <jgriffin3@harding.edu>
2017-09-19 16:12:50 -0500

    Created Project
    Created Android Studio project and added Dots.java and DotsGame.java.

---
+++ app/src/main/java/edu/harding/dots/Dot.java
@@ -0,0 +1,58 @@
+package edu.harding.dots;
+
+import java.util.Random;
+
+/**
+ * Created by fmccown on 9/4/2017.
+ */
+
+public class Dot {
+    public int color;
+    public int row;
+    public int col;
+    public boolean selected;
+
+    private Random randomGen;
+
+    public Dot(int row, int col) {
+        randomGen = new Random();
+        color = randomGen.nextInt(DotsGame.NUM_COLORS);
+        selected = false;
+        this.row = row;
+        this.col = col;
+    }
+
+    private void setColor() {
+        color = randomGen.nextInt(DotsGame.NUM_COLORS);
+    }
+
+    public boolean isAdjacent(Dot lastDot) {
+        int colDiff = Math.abs(this.col - lastDot.col);
+        int rowDiff = Math.abs(this.row - lastDot.row);
+        return (colDiff + rowDiff == 1);
+    }
+
+    public void changeColor() {
+        setColor();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Dot dot = (Dot) o;
+
+        if (row != dot.row) return false;
+        return col == dot.col;
+    }
+
+    @Override
+    public String toString() {
+        return "Dot{" +
+                "color=" + color +
+                ", row=" + row +
+                ", col=" + col +
+                '}';
+    }
+}
