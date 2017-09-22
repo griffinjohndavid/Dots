0ad308a3ed363d042ac3da91b80c68e385bfb9c7
John Griffin <jgriffin3@harding.edu>
2017-09-22 12:13:31 -0500

    Created Main Layout
    Created layout for activity_main and created icons for the app.

--- app/src/main/java/edu/harding/dots/MainActivity.java
+++ app/src/main/java/edu/harding/dots/MainActivity.java
@@ -2,6 +2,7 @@ package edu.harding.dots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // test
+
+    public void timedGameClick(View view) {
+
+    }
+
+    public void movesGameClick(View view) {
+
+    }
 }
